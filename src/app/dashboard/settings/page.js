cf9a0811b2b9d8203960768e0eff1eb6026e9ed8
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-02-20 21:26:12 +0600

    first commit

---
+++ src/app/dashboard/settings/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const componentName = () => {
+    return (
+        <div>
+            This settings page
+        </div>
+    );
+};
+
+export default componentName;
