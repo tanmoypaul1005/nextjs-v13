13d8d221da5e49e05543672708c812da7a68c0c3
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-02-21 13:07:26 +0600

    fetch data

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,26 +1,32 @@
 import Link from 'next/link'
 import React, { Suspense } from 'react'
 
-const fetchData = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = res.json();
-  // throw new Error("Error from get data")
-  return data;
+async function getData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  // fetch('https://...', { cache: 'no-store' })
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
 }
 
-export default async function page() {
-  const data = await fetchData();
-  return (
-    <div>
-      <Suspense fallback={<div>Loading.........................</div>}>
-      {data?.map((item, index) => (
-        <>
-          <Link href={`users/${item?.id}`}>{item?.name} {item?.id}</Link>
-          <br></br>
-        </>
-      ))}
-      </Suspense>
-    </div>
-    
-  )
+export default async function Page() {
+  const data = await getData();
+
+  return <div>
+  <Suspense fallback={<div>Loading.........................</div>}>
+  {data?.map((item, index) => (
+    <>
+      <Link href={`users/${item?.id}`}>{item?.name} ffffff{item?.id}</Link>
+      <br></br>
+    </>
+  ))}
+  </Suspense>
+</div>
 }
