d24492837eb5cfb282033d3861b304b1c7c75a5b
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-02-21 10:38:39 +0600

    code remove

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -22,11 +22,6 @@ export default async function page() {
           <br></br>
         </>
       ))}
-
-      {/* <Link href="users/2">user2 </Link>
-      <Link href="users/3">user3 </Link>
-      <Link href="users/4">user4 </Link>
-      <Link href="users/5">user5 </Link> */}
       </Suspense>
     </div>
     
